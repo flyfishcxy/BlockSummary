b5445141d94b0fbd6618688e145b5addf643e0ee
chenxiaoye <chenxiaoye@damai.cn>
2017-04-27 18:02:25 +0800

    提交bloc代码

---
+++ BlockSummary/BlockDemo/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BlockDemo
+//
+//  Created by chen on 2017/4/27.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ BlockSummary/BlockSummary/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlockSummary
+//
+//  Created by chen on 2017/4/27.
+//  Copyright © 2017年 chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
